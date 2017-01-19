5f35e731f3ded937e6930cfcabc7f0e111c46ede
Daniel Masson <dmasson@users.noreply.github.com>
2017-01-19 12:30:44 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ configure_aws_cli(){
 	aws --version
 	aws configure set default.aws_access_key_id $ENV1
    	aws configure set default.aws_secret_access_key $ENV2
-	aws configure set default.region us-east-1
+	aws configure set default.region us-west-2
 	aws configure set default.output json
 }
 
